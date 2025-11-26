bdcd3f1ff95b278de4343a400266196dd4262a8e
Matvei4ik1k <Matvei4ik1k@WIN10>
2025-11-26 17:59:12 +0300

    ...

--- Zachet_Mirzaeva/Windows/RegistrationWindow.xaml.cs
+++ Zachet_Mirzaeva/Windows/RegistrationWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,20 +18,11 @@ namespace Zachet_Mirzaeva.Windows
     /// <summary>
     /// Логика взаимодействия для RegistrationWindow.xaml
     /// </summary>
-    public partial class RegistrationWindow : Window
+    public partial class RegistrationWindow : MetroWindow
     {
         public RegistrationWindow()
         {
             InitializeComponent();
-        }
-
-      
-
-        private void AuthorizationBtn_Click(object sender, RoutedEventArgs e)
-        {
-RegistrationWindow registrationWindow = new RegistrationWindow();
-            registrationWindow.Show();
-            this.Close();
         }
     }
 }
